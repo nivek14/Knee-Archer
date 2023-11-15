14ab16dd6fdcb5e6ab9284c6852b3ec224b5375c
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-11-15 03:44:46 -0300

    feat: primeira versão do script de movimentação do arco

--- Assets/Scripts/Player/MoveBow.cs
+++ Assets/Scripts/Player/MoveBow.cs
@@ -1,25 +1,28 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveBow : MonoBehaviour{
 
-    [SerializeField] private GameObject player;
-    private Vector3 vector3;
-    private float angle;
-    private float distance = .18f;
+    public Transform orb;
+    public float radius;
+ 
     private Transform pivot;
-
-    void Start(){
-        pivot = player.transform;
-        this.transform.parent = pivot;
-        this.transform.position = vector3.up * distance;
+ 
+    void Start()
+    {
+        pivot = orb.transform;
+        transform.parent = pivot;
+        transform.position += Vector3.up * radius;
     }
-
-    
-    void Update(){
-        
+ 
+    void Update()
+    {
+        Vector3 orbVector = Camera.main.WorldToScreenPoint(orb.position);
+        orbVector = Input.mousePosition - orbVector;
+        float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+ 
+        pivot.position = orb.position;
+        pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
-
-
-}*/
+}

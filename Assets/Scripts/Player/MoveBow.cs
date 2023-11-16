f1c2caf69482528f673e0464438b63b68d1f7e39
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-11-16 12:15:51 -0300

    feat: refactor no movimento do arco

--- Assets/Scripts/Player/MoveBow.cs
+++ Assets/Scripts/Player/MoveBow.cs
@@ -7,8 +7,15 @@ public class MoveBow : MonoBehaviour{
     public Transform gunHolder;
  
     void Update(){
+        BowMovement();
+    }
+
+    void BowMovement(){
+         // inputs do mouse
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+        // ratação do arco
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunHolder.rotation = Quaternion.Euler(0,0,angle);

27b2cb75a3728fdee5d5cd3242fd45ea9460bc8a
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-11-17 16:54:09 -0300

    feat: limitando o movimento em 180 graus

--- Assets/Scripts/Player/MoveBow.cs
+++ Assets/Scripts/Player/MoveBow.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class MoveBow : MonoBehaviour{
 
     public Transform gunHolder;
+    public float yPivot;
  
     void Update(){
         BowMovement();
     }
 
     void BowMovement(){
-         // inputs do mouse
+        // inputs do mouse
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
         // rotação do arco
-        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, -yPivot);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        angle = Mathf.Clamp(angle, -180, 0);
         gunHolder.rotation = Quaternion.Euler(0,0,angle);
+        Debug.Log(angle);
     }
     
 }

3ca83304052c0591880bbe9b2eb2acfae93962cd
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-11-16 22:54:29 -0300

    feature: alguns pequenos ajustes

--- Assets/Scripts/Player/MoveBow.cs
+++ Assets/Scripts/Player/MoveBow.cs
@@ -14,10 +14,10 @@ public class MoveBow : MonoBehaviour{
          // inputs do mouse
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-
-        // ratação do arco
+        // rotação do arco
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunHolder.rotation = Quaternion.Euler(0,0,angle);
     }
+    
 }
